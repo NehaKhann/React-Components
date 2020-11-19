b7c460d2a271c2a0573dee6a76d452cfcd9c2545
NehaKhann <n.nehakhan333@gmail.com>
2020-11-19 15:50:35 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.jsx";
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
